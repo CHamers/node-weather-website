8972b54a535d7275d7aa2eb92bae42ba2559b8e9
Christel Hamers <christelhamers@gmail.com>
2023-03-11 11:32:56 +0000

    Correct spelling mistake and add 'about' text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -27,7 +27,7 @@ const forecast = (latitude, longitude, callback) => {
         body.current.weather_descriptions[0] +
           ". It is currently " +
           body.current.temperature +
-          " degress out."
+          " degrees Celsius."
       );
     }
   });
